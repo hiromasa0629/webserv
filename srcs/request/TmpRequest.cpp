7196a376ad256bfa5a5e9a95611400491022fa1d
Hiromasa <howzheng0629@gmail.com>
2023-03-04 18:00:25 +0800

    tmprequest

--- srcs/request/TmpRequest.cpp
+++ srcs/request/TmpRequest.cpp
@@ -6,15 +6,15 @@
 /*   By: hyap <hyap@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:56:35 by hyap              #+#    #+#             */
-/*   Updated: 2023/03/03 22:51:35 by hyap             ###   ########.fr       */
+/*   Updated: 2023/03/04 17:37:19 by hyap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "TmpRequest.hpp"
 
-const char	TmpRequest::_methods[] = {"GET"};
+const char	TmpRequest::_methods[] = {"GET", "POST", "DELETE"};
 
-TmpRequest::TmpRequest(void) : _is_complete(false), _req("") {}
+TmpRequest::TmpRequest(void) : _is_complete(false), _is_complete_header(false), _req("") {}
 
 TmpRequest::~TmpRequest(void) {} 
 
@@ -31,21 +31,29 @@ bool	TmpRequest::get_is_complete(void) const
 void	TmpRequest::append(const std::string& req)
 {
 	this->_req.append(req);
-	if (!this->check_header_complete())
-		return ;
-	
+	if (!_is_complete_header)
+	{
+		if (!this->check_header_complete())
+			return ;
+		this->extract_header_info();
+		this->_is_complete_header = true;
+	}
+	if (this->_header_info["method"] == "POST")
+		this->handle_post(req);
+	else
+		this->handle_get();
 }
 
 bool	TmpRequest::check_header_complete(void)
 {
 	if (this->_req.length() < 4)
-		return (false);
+		throw RequestErrorException(__LINE__, 400, "Invalid request length");
 	if (this->_req.find("\r\n\r\n") == std::string::npos)
 		return (false);
 	return (true);
 }
 
-void	TmpRequest::extract_header_info(void)
+bool	TmpRequest::extract_header_info(void)
 {
 	size_t			index;
 	std::string		header;
@@ -60,11 +68,177 @@ void	TmpRequest::extract_header_info(void)
 		header = std::string(header.begin() + index + 2, header.end());
 	}
 	split = utils::ft_split(header_lines[0]);
-	
-	for (size_t i = 0; i < header_lines.size(); i++)
+	if (!this->check_request_line(split))
+		throw RequestErrorException(__LINE__, 400, "Request line error");
+	this->_header_info.insert(std::make_pair("method", split[0]));
+	this->_header_info.insert(std::make_pair("uri", split[1]));
+	this->_header_info.insert(std::make_pair("protocol", split[2]));
+	for (size_t i = 1; i < header_lines.size(); i++)
 	{
 		split = utils::ft_split(header_lines[i]);
-		
+		if (split.front() == "Host:")
+		{
+			this->_header_info.insert(std::make_pair("server_name", split[1].substr(0, split[1].find_first_of(':'))));
+			this->_header_info.insert(std::make_pair("port", split[1].substr(split[1].find_first_of(':') + 1)));
+		}
+		else if (split.front() == "Content-Type:")
+			this->handle_content_type(utils::StrVec(split.begin() + 1, split.end()));
+		else if (split.front() == "Transfer-Encoding:")
+			this->_header_info.insert(std::make_pair("transfer-encoding", split.back()));
+		else if (split.front() == "Content-Length:")
+			this->_header_info.insert(std::make_pair("content-length", split.back()));
 	}
+	if (this->_header_info.find("server_nama") == this->_header_info.end())
+		throw RequestErrorException(__LINE__, 400, "Missing request host");
+}	
+
+bool	TmpRequest::check_request_line(const utils::StrVec& vec) const
+{
+	int	i;
 	
+	if (vec.empty())
+		return (false);
+	if (vec.size() < 3)
+		return (false);
+	i = 0;
+	while (i < 3)
+	{
+		if (TmpRequest::_methods[i] == vec.front())
+			break ;
+		i++;
+	}
+	if (i == 3)
+		return (false);
+	if (vec.back() != "HTTP/1.1")
+		return (false);
+	if (vec[1].front() != '/')
+		return (false);
+	return (true);
 }
+
+void	TmpRequest::handle_content_type(const utils::StrVec& val)
+{
+	std::string	type;
+	
+	type = val.front();
+	if (type.back() == ';')
+		type.pop_back();
+	this->_header_info.insert(std::make_pair("content-type", type));
+	if (type == "mutlipart/form-data")
+	{
+		if (val.size() != 2)
+			throw RequestErrorException(__LINE__, 400, "Invalid form-data");
+		else
+			this->_header_info.insert(std::make_pair("boundary", val.back().substr(val.back().find_first_of('=') + 1)));
+	}
+}
+
+void	TmpRequest::handle_get(void)
+{
+	std::string	uri;
+	std::string	query;
+	int			index;
+	
+	uri = this->_header_info["uri"];
+	if (uri.find_first_of('?') == std::string::npos)
+		return ;
+	query = uri.substr(uri.find_first_of('?') + 1);
+	while ((index = query.find_first_of('&')) != std::string::npos)
+	{
+		std::string	single;
+		int			index_two;
+		
+		single = query.substr(0, index);
+		index_two = single.find_first_of('=');
+		this->_query.insert(std::make_pair(single.substr(0, index_two), single.substr(index_two + 1)));
+		query = query.substr(index + 1);
+	}
+}
+
+void	TmpRequest::handle_post(const std::string& req)
+{
+	if (this->_header_info.count("transfer-encoding") > 0 && this->_header_info["transfer-encoding"] == "chunked")
+	{
+		if (this->_chunked_body.empty())
+			this->_chunked_body = this->_req.substr(this->_req.find("\r\n\r\n") + 4);
+		else
+			this->_chunked_body.append(req);
+		if (this->_chunked_body.find("\r\n0\r\n\r\n"));
+		{
+			this->_is_complete = true;
+			this->_body = this->tidy_up_chunked_body();
+		}
+	}
+	else if (this->_header_info.count("content-length") > 0)
+	{
+		int			content_length;
+		
+		content_length = std::stoi(this->_header_info["content-length"]);
+		if (this->_body.empty())
+		{
+			std::string body;
+			
+			body = this->_req.substr(this->_req.find("\r\n\r\n") + 4);
+			if (body.size() > content_length)
+				this->_body.append(body, content_length);
+			else
+				this->_body.append(body);
+		}
+		else
+		{
+			int	remaining;
+			
+			remaining = content_length - this->_body.size();
+			if (req.size() > remaining)
+				this->_body.append(req, remaining);
+			else
+				this->_body.append(req);
+		}
+		if (this->_body.size() == content_length)
+			this->_is_complete = true;
+	}
+}
+
+std::string	TmpRequest::tidy_up_chunked_body(void)
+{
+	utils::StrVec	lines;
+	std::string		body;
+	size_t			index;
+	int				size;
+	std::string		res;
+	
+	body = this->_chunked_body;
+	while ((index = body.find("\r\n")) != std::string::npos)
+	{
+		lines.push_back(body.substr(0, index));
+		body = body.substr(index + 2);
+	}
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		size = std::stoi(lines[i++]);
+		for (int j = 0; j < size; j++)
+			res.push_back(lines[i][j]);
+	}
+	return (res);
+}
+
+TmpRequest::RequestErrorException::RequestErrorException(int line, int status, const std::string& msg)
+	: _line(line), _status(status), _msg(msg) {}
+
+TmpRequest::RequestErrorException::~RequestErrorException(void) throw() {}
+
+const char*	TmpRequest::RequestErrorException::what(void) const throw()
+{
+	std::stringstream	ss;
+	
+	ss << this->_status << " ";
+	ss << this->_msg << " ";
+	ss << "line: " << this->_line;
+	return (ss.str().c_str());
+}
+
+int	TmpRequest::RequestErrorException::get_status(void) const throw()
+{
+	return (this->_status);
+}
+
--- srcs/request/TmpRequest.hpp
+++ srcs/request/TmpRequest.hpp
@@ -6,7 +6,7 @@
 /*   By: hyap <hyap@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:53:38 by hyap              #+#    #+#             */
-/*   Updated: 2023/03/03 22:50:22 by hyap             ###   ########.fr       */
+/*   Updated: 2023/03/04 17:13:47 by hyap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <map>
 # include <iterator>
 # include "utils.hpp"
+# include <exception>
+# include <sstream>
 
 class TmpRequest {
 	public:
@@ -25,17 +27,122 @@ class TmpRequest {
 		std::string	get_req(void) const;
 		bool		get_is_complete(void) const;
 		
+		/**
+		 * @brief append received bytes from recv() into _req.
+		 * 
+		 * @param req recv() received bytes
+		 */
 		void		append(const std::string& req);
 		
+		class RequestErrorException : public std::exception {
+			public:
+				RequestErrorException(int line, int status, const std::string& msg);
+				virtual ~RequestErrorException(void) throw();
+				virtual const char*	what(void) const throw();
+				int					get_status(void) const throw();
+			private:
+				int			_line;
+				int			_status;
+				std::string	_msg;
+		};
+		
 	private:
+
+		/**
+		 * @brief if _req length is less than 4, throws exception. Check if header is terminated with crlf
+		 * 
+		 * @return true if above condition fulfilled
+		 * @return false if one of the condition did not meet
+		 */
 		bool			check_header_complete(void);
-		void			extract_header_info(void);
+		
+		/**
+		 * @brief Extract header info into _header_info
+		 * 
+		 * @return true if successful extract all necessary fileds eg. method, uri, host, protocol
+		 * @return false if one of them is missing
+		 */
+		bool			extract_header_info(void);
+		
+		/**
+		 * @brief Check request line if method, uri, protocol exsists and correct
+		 * 
+		 * @param vec first line of request header
+		 * @return true if condition fulfilled
+		 * @return false if failed one of the condition
+		 */
+		bool			check_request_line(const utils::StrVec& vec) const;
 	
+	
+		/**
+		 * @brief handle the value of content-type by determine chunked and multipart/form-data
+		 * 
+		 * @param val vector of value of content-type
+		 */
+		void			handle_content_type(const utils::StrVec& val);
+		
+		/**
+		 * @brief handle GET request. Save query into _query
+		 */
+		void			handle_get(void);
+		
+		/**
+		 * @brief handle POST request. Determine chunked or normal and save body into _body
+		 * 
+		 * @param req btyes received by recv()
+		 */
+		void			handle_post(const std::string& req);
+		
+		/**
+		 * @brief read the chunked body and save them into _req
+		 * 
+		 * @return body without chunked header
+		 */
+		std::string			tidy_up_chunked_body(void);
+
+		/**
+		 * @brief indicates that this request is complete
+		 */
 		bool								_is_complete;
+		
+		/**
+		 * @brief indicates that header is completely received
+		 */
+		bool								_is_complete_header;
+		
+		/**
+		 * @brief consist of request heaeder and body
+		 */
 		std::string							_req;
+		
+		/**
+		 * @brief final request body
+		 */
+		std::string							_body;
+		
+		/**
+		 * @brief save untidy chunked body
+		 */
+		std::string							_chunked_body;
+		
+		/**
+		 * @brief request header
+		 */
 		std::string							_header;
+		
+		/**
+		 * @brief map of <request fields, value>
+		 */
 		std::map<std::string, std::string>	_header_info;
 		
+		/**
+		 * @brief GET request qeury
+		 */
+		std::map<std::string, std::string>	_query;
+		
+		/**
+		 * @brief static strings to validate request methods
+		 */
 		static const char					_methods[];
 };
 
